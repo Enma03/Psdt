57dd4f447aab1d6fdc7e1d7f1b34bbd786539467
Enma03 <edjehm1@gmail.com>
2023-08-22 17:42:41 -0500

    version 2.0
    esta version, es la informacion que se uso en mysql, y ahora esta en acces, y se hace llamado a esta

--- pruebas acces/conexion.py
+++ pruebas acces/conexion.py
@@ -1,8 +1,9 @@
 import sys
 import pyodbc
-conn= pyodbc.connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\ASUS\Documents\GitHub\pruebas acces\Database1.accdb;")
+conn= pyodbc.connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\ASUS\Documents\GitHub\buenas practicas\Psdt\pruebas acces\Database1.accdb;")
 cursor=conn.cursor()
-cursor.execute("SELECT * FROM persona")
+cursor.execute("SELECT * FROM AsgEst ORDER BY id ASC")
+print("(ID, 'NOTA1', 'NOTA2', 'NOTA3', 'idAsignatura', 'idEstudiante', 'FECHA')")
 for row in cursor.fetchall():
     print(row)
 cursor.close
